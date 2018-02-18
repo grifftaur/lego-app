4f5fe5b68d8b4ea1ce22ae8a43cf5612d1df7720
grifftaur <eder287@gmail.com>
2018-02-18 14:31:19 -0500

    setup S3 for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
 config.action_mailer.default_url_options = { host: 'howlongtobuild.herokuapp.com' }
 
 config.action_mailer.delivery_method = :smtp
@@ -17,7 +18,15 @@ enable_starttls_auto: true,
 user_name: ENV["GMAIL_USERNAME"],
 password: ENV["GMAIL_PASSWORD"]
 }
-
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 
 Rails.application.routes.default_url_options[:host] = 'howlongtobuild.herokuapp.com'
   # Code is not reloaded between requests.
